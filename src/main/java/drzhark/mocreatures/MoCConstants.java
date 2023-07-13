92383b9934549decd4a2d0a8ad08813d24a0df70
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-07-13 21:30:02 +0200

    Bump version

--- src/main/java/drzhark/mocreatures/MoCConstants.java
+++ src/main/java/drzhark/mocreatures/MoCConstants.java
@@ -8,8 +8,8 @@ public class MoCConstants {
     public static final String MOD_ID = "mocreatures";
     public static final String MOD_PREFIX = MOD_ID + ":";
     public static final String MOD_NAME = "DrZhark's Mo'Creatures Extended";
-    public static final String MOD_VERSION = "12.1.2";
-    public static final String MOD_ACCEPTED_VERSIONS = "[12.1.2]";
+    public static final String MOD_VERSION = "12.2.0";
+    public static final String MOD_ACCEPTED_VERSIONS = "[" + MOD_VERSION + "]";
     public static final String MOD_DEPENDENCIES = "after:futuremc;after:thaumcraft;after:thermalexpansion;after:traverse";
     public static final Integer DATAFIXER_VERSION = 1;
 }

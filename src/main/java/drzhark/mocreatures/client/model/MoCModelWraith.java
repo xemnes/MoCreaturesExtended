03a036caa6f679c4b1c1e468c7a39e46826bc7ff
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-07-24 19:32:29 +0200

    Inherit biped rotation angles for wraiths

--- src/main/java/drzhark/mocreatures/client/model/MoCModelWraith.java
+++ src/main/java/drzhark/mocreatures/client/model/MoCModelWraith.java
@@ -39,7 +39,6 @@ public class MoCModelWraith extends ModelBiped {
     public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
         this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entityIn);
         GlStateManager.pushMatrix();
-
         if (this.isChild) {
             GlStateManager.scale(0.75F, 0.75F, 0.75F);
             GlStateManager.translate(0.0F, 16.0F * scale, 0.0F);
@@ -48,35 +47,26 @@ public class MoCModelWraith extends ModelBiped {
             GlStateManager.pushMatrix();
             GlStateManager.scale(0.5F, 0.5F, 0.5F);
             GlStateManager.translate(0.0F, 24.0F * scale, 0.0F);
-            this.bipedBody.render(scale);
-            this.bipedRightArm.render(scale);
-            this.bipedLeftArm.render(scale);
-            this.bipedRightLeg.render(scale);
-            this.bipedLeftLeg.render(scale);
         } else {
             if (entityIn.isSneaking()) {
                 GlStateManager.translate(0.0F, 0.2F, 0.0F);
             }
-
             this.bipedHead.render(scale);
-            this.bipedBody.render(scale);
-            this.bipedRightArm.render(scale);
-            this.bipedLeftArm.render(scale);
-            this.bipedRightLeg.render(scale);
-            this.bipedLeftLeg.render(scale);
         }
-
+        this.bipedBody.render(scale);
+        this.bipedRightArm.render(scale);
+        this.bipedLeftArm.render(scale);
+        this.bipedRightLeg.render(scale);
+        this.bipedLeftLeg.render(scale);
         GlStateManager.popMatrix();
     }
 
-    // TODO: Fix this
     @Override
     public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity par7Entity) {
-
+        super.setRotationAngles(f, f1, f2, f3, f4, f5, par7Entity);
         if (par7Entity instanceof MoCEntityWraith) {
             this.attackCounter = ((MoCEntityWraith) par7Entity).attackCounter;
         }
-
         float f6 = MathHelper.sin(this.swingProgress * 3.141593F);
         float f7 = MathHelper.sin((1.0F - ((1.0F - this.swingProgress) * (1.0F - this.swingProgress))) * 3.141593F);
         this.bipedRightArm.rotateAngleZ = 0.0F;
@@ -85,7 +75,6 @@ public class MoCModelWraith extends ModelBiped {
         this.bipedLeftArm.rotateAngleY = 0.1F - (f6 * 0.6F);
         if (this.attackCounter != 0) {
             float armMov = (MathHelper.cos((attackCounter) * 0.12F) * 4F);
-
             this.bipedRightArm.rotateAngleX = -armMov;
             this.bipedLeftArm.rotateAngleX = -armMov;
         } else {
@@ -96,7 +85,6 @@ public class MoCModelWraith extends ModelBiped {
             this.bipedRightArm.rotateAngleX += MathHelper.sin(f2 * 0.067F) * 0.05F;
             this.bipedLeftArm.rotateAngleX -= MathHelper.sin(f2 * 0.067F) * 0.05F;
         }
-
         this.bipedRightArm.rotateAngleZ += (MathHelper.cos(f2 * 0.09F) * 0.05F) + 0.05F;
         this.bipedLeftArm.rotateAngleZ -= (MathHelper.cos(f2 * 0.09F) * 0.05F) + 0.05F;
     }

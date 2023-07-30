159e01c664c6d66679d2c3b542b07b42c1b04a26
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-07-30 20:19:17 +0200

    Implement MIA config handling
    Scuffed but a quick solution for MIA compatibility woes

--- src/main/java/drzhark/mocreatures/compat/CompatHandler.java
+++ src/main/java/drzhark/mocreatures/compat/CompatHandler.java
@@ -14,14 +14,53 @@ import drzhark.mocreatures.compat.thaumcraft.ThaumcraftIntegration;
 import drzhark.mocreatures.compat.thermalexpansion.ThermalExpansionIntegration;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.launchwrapper.Launch;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
 @Mod.EventBusSubscriber(modid = MoCConstants.MOD_ID)
 public class CompatHandler {
+
+    static {
+        try {
+            File file = new File(Launch.minecraftHome, "config" + File.separator + "mia" + File.separator + "mocreatures.cfg");
+            if (Files.exists(file.toPath())) {
+                File tempFile = new File(Launch.minecraftHome, "config" + File.separator + "mia" + File.separator + "mocreatures_temp.cfg");
+                List<String> configEntries = new ArrayList<>();
+                configEntries.add("Enable FutureMC integration");
+                configEntries.add("Enable Hatchery integration");
+                configEntries.add("Enable Ice and Fire additions");
+                configEntries.add("Enable Industrial Foregoing integration");
+                configEntries.add("Enable JER integration");
+                configEntries.add("Enable Thermal Expansion integration");
+                configEntries.add("Increase damage to werewolves from other mod silver weapons");
+                configEntries.add("Replace cod and clownfish drops with their corresponding item");
+                try (BufferedReader br = new BufferedReader(new FileReader(file)); BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile))) {
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        for (String targetConfigEntry : configEntries) {
+                            if (line.contains(targetConfigEntry)) {
+                                line = line.replace("=true", "=false");
+                            }
+                        }
+                        bw.write(line + "\n");
+                    }
+                }
+                file.delete();
+                tempFile.renameTo(file);
+                System.out.println("MIA config file modified successfully!");
+            }
+        } catch (Exception ignored) {
+        }
+    }
 
     @SubscribeEvent
     public static void registerRecipes(RegistryEvent.Register<IRecipe> event) {

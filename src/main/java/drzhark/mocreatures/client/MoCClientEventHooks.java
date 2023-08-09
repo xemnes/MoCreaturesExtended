2d2fdb5da1c2919c67fde6cf2151d31830835bff
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-08-09 15:50:39 +0200

    Revert scorpion climbing rotation
    Closes #60
    Closes #63

--- src/main/java/drzhark/mocreatures/client/MoCClientEventHooks.java
+++ src/main/java/drzhark/mocreatures/client/MoCClientEventHooks.java
@@ -4,50 +4,23 @@
 package drzhark.mocreatures.client;
 
 import drzhark.mocreatures.compat.CompatScreen;
-import drzhark.mocreatures.entity.hostile.MoCEntityScorpion;
-import drzhark.mocreatures.entity.hunter.MoCEntityPetScorpion;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiMainMenu;
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.EnumFacing;
 import net.minecraftforge.client.event.GuiOpenEvent;
-import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.Type;
 
 public class MoCClientEventHooks {
 
-    boolean inMenu;
-    int lastTickRun;
-
-    private void onTickInGame() {
-    }
-
-    private void onTickInGui(GuiScreen curScreen) {
-        // handle GUI tick stuff here
-        this.inMenu = true;
-        this.lastTickRun = 0;
-    }
-
-    @SubscribeEvent
-    public void tickEnd(ClientTickEvent event) {
-        if (event.type.equals(Type.CLIENT)) {
-            GuiScreen curScreen = Minecraft.getMinecraft().currentScreen;
-            if (curScreen != null) {
-                onTickInGui(curScreen);
-            } else {
-                this.inMenu = false;
-                onTickInGame();
-            }
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void displayCompatScreen(GuiOpenEvent event) {
+        if (event.getGui() instanceof GuiMainMenu && CompatScreen.showScreen && Loader.isModLoaded("customspawner")) {
+            event.setGui(new CompatScreen());
+            CompatScreen.showScreen = false;
         }
     }
 
+    /* TODO: Fix rider rotation
     @SubscribeEvent
     public void renderClimbingRiderPre(RenderLivingEvent.Pre<EntityLivingBase> event) {
         EntityLivingBase rider = event.getEntity();
@@ -75,15 +48,21 @@ public class MoCClientEventHooks {
         EntityLivingBase rider = event.getEntity();
         Entity mount = rider.getRidingEntity();
         if (mount instanceof MoCEntityScorpion || mount instanceof MoCEntityPetScorpion) {
-            GlStateManager.popMatrix();
+            if (((EntityLivingBase) rider.getRidingEntity()).isOnLadder()) {
+                EnumFacing facing = rider.getHorizontalFacing();
+                GlStateManager.translate(0.0F, 1.0F, 0.0F);
+                if (facing == EnumFacing.NORTH) {
+                    GlStateManager.rotate(90.0F, -1.0F, 0.0F, 0.0F);
+                } else if (facing == EnumFacing.WEST) {
+                    GlStateManager.rotate(90.0F, 0.0F, 0.0F, 1.0F);
+                } else if (facing == EnumFacing.SOUTH) {
+                    GlStateManager.rotate(90.0F, 1.0F, 0.0F, 0.0F);
+                } else if (facing == EnumFacing.EAST) {
+                    GlStateManager.rotate(90.0F, 0.0F, 0.0F, -1.0F);
+                }
+                GlStateManager.popMatrix();
+            }
         }
     }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void displayCompatScreen(GuiOpenEvent event) {
-        if (event.getGui() instanceof GuiMainMenu && CompatScreen.showScreen && Loader.isModLoaded("customspawner")) {
-            event.setGui(new CompatScreen());
-            CompatScreen.showScreen = false;
-        }
-    }
+    */
 }

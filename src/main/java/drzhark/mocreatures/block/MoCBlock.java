093f76b04774e8491dc7deebfc5407af383deadd
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-08-28 09:25:16 +0200

    Makeshift MIA compatibility

---
+++ src/main/java/drzhark/mocreatures/block/MoCBlock.java
@@ -0,0 +1,14 @@
+/*
+ * GNU GENERAL PUBLIC LICENSE Version 3
+ */
+package drzhark.mocreatures.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+// Just here for makeshift MIA compatibility
+public class MoCBlock extends Block {
+    public MoCBlock(Material materialIn) {
+        super(materialIn);
+    }
+}
